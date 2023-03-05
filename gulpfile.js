9cb5232255e7f8ed1676983706a8955b6c9d7a8f
vidget38 <vidget38@mail.ru>
2023-03-05 10:56:48 +0300

    update: add packages to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,48 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, parallel} = require('gulp');
 
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('autoprefixer');
 
 function scripts() {
-     return src('app/js/main.js')
+     return src([
+        'node_modules/swiper/swiper-bundle.js',
+        'app/js/main.js',
+    ])
        .pipe(concat('main.min.js'))
        .pipe(uglify())
        .pipe(dest('app/js'))
+       .pipe(browserSync.stream())
 }
 
 function styles() {
     return src('app/scss/style.scss')
+      .pipe(autoprefixer({overrideBrowserslist: ['last 10 version']}))
       .pipe(concat('style.min.css'))
       .pipe(scss({outputStyle: 'compressed'}))
       .pipe(dest('app/css'))
+      .pipe(browserSync.stream())
 }
 
 function watching() {
     watch(['app/scss/style.scss'], styles)
     watch(['app/js/main.js'], scripts)
+    watch(['app/*.html']).on('change', browserSync.reload);
+}
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: 'app/'
+        }
+    });
 }
 
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.defaults = parallel(styles, scripts, browsersync, watching);
